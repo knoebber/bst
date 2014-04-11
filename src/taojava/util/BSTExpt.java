4bbf9a4856ae90814247bf7963a2d4a0f23a6f92
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2014-04-11 18:04:13 -0500

    Reformatted code.  (Did this yesterday, forgot to commit and push.)

--- src/taojava/util/BSTExpt.java
+++ src/taojava/util/BSTExpt.java
@@ -11,80 +11,90 @@ import java.util.Iterator;
  * @author Samuel A. Rebelsky
  * @author Your Name Here
  */
-public class BSTExpt {
-    // +--------+----------------------------------------------------------
-    // | Fields |
-    // +--------+
+public class BSTExpt
+{
+  // +--------+----------------------------------------------------------
+  // | Fields |
+  // +--------+
 
-    /**
-     * Output for our program.
-     */
-    static PrintWriter pen;
+  /**
+   * Output for our program.
+   */
+  static PrintWriter pen;
 
-    /**
-     * The current dictionary.
-     */
-    static BST<String,String> dict;
+  /**
+   * The current dictionary.
+   */
+  static BST<String, String> dict;
 
-    // +------+------------------------------------------------------------
-    // | Main |
-    // +------+
+  // +------+------------------------------------------------------------
+  // | Main |
+  // +------+
 
-    public static void main(String[] args) throws Exception {
-        pen = new PrintWriter(System.out, true);
-        dict = new BST<String,String>(new Comparator<String>() {
-                   public int compare(String left, String right) {
-                       return left.compareTo(right);
-                   } // compare(String, String)
-                });
-        dict.dump(pen);
-        pen.println();
-        expt("e", "elephant");
-        expt("c", "chinchilla");
-        expt("b", "baboon");
-        expt("d", "dingo");
-        expt("a", "aardvark");
-        expt("g", "gibbon");
-        expt("h", "hippo");
-        // expt("a", "ant");
-        // expt("z", "chinchilla");
-    } // main(String[])
+  public static void main(String[] args)
+    throws Exception
+  {
+    pen = new PrintWriter(System.out, true);
+    dict = new BST<String, String>(new Comparator<String>()
+      {
+        public int compare(String left, String right)
+        {
+          return left.compareTo(right);
+        } // compare(String, String)
+      });
+    dict.dump(pen);
+    pen.println();
+    expt("e", "elephant");
+    expt("c", "chinchilla");
+    expt("b", "baboon");
+    expt("d", "dingo");
+    expt("a", "aardvark");
+    expt("g", "gibbon");
+    expt("h", "hippo");
+    // expt("a", "ant");
+    // expt("z", "chinchilla");
+  } // main(String[])
 
-    // +-----------+-------------------------------------------------------
-    // | Utilities |
-    // +-----------+
+  // +-----------+-------------------------------------------------------
+  // | Utilities |
+  // +-----------+
 
-    /**
-     * Add a key/value pair to the BST then print it out.
-     */
-    public static void expt(String key, String value) {
-        pen.println("dict[" + key + "] = " + value);
-        dict.set(key,value);
-        // iterateKeys();
-        // iterateValues();
-        dict.dump(pen);
-        pen.println();
-    } // expt(BST, PrintWriter, String, String)
+  /**
+   * Add a key/value pair to the BST then print it out.
+   */
+  public static void expt(String key, String value)
+  {
+    pen.println("dict[" + key + "] = " + value);
+    dict.set(key, value);
+    // iterateKeys();
+    // iterateValues();
+    dict.dump(pen);
+    pen.println();
+  } // expt(BST, PrintWriter, String, String)
 
-    /**
-     * Iterate the keys of the dictionary.
-     */
-    public static void iterateKeys() {
-        pen.print("Keys:   ");
-        for (String key : dict.keys()) {
-            pen.print(key + " ");
-        } // for each key
-        pen.println();
-    } // iterateKeys()
+  /**
+   * Iterate the keys of the dictionary.
+   */
+  public static void iterateKeys()
+  {
+    pen.print("Keys:   ");
+    for (String key : dict.keys())
+      {
+        pen.print(key + " ");
+      } // for each key
+    pen.println();
+  } // iterateKeys()
 
-    /**
-     * Iterate the values of the dictionary.
-     */
-    public static void iterateValues() {
-        pen.print("Values: ");
-        for (String value : dict) {
-            pen.print(value + " ");
-        } // for each value
-        pen.println();
-    } // iterateValues
+  /**
+   * Iterate the values of the dictionary.
+   */
+  public static void iterateValues()
+  {
+    pen.print("Values: ");
+    for (String value : dict)
+      {
+        pen.print(value + " ");
+      } // for each value
+    pen.println();
+  } // iterateValues
 } // BSTExpt
--- src/taojava/util/Dictionary.java
+++ src/taojava/util/Dictionary.java
@@ -9,72 +9,76 @@ import java.util.Iterator;
  * @author Samuel A. Rebelsky
  * @author CSC 207 2013F
  */
-public interface Dictionary<K,V> extends Iterable<V> {
+public interface Dictionary<K, V>
+    extends Iterable<V>
+{
 
-    // +-----------+-------------------------------------------------------
-    // | Observers |
-    // +-----------+
+  // +-----------+-------------------------------------------------------
+  // | Observers |
+  // +-----------+
 
-    /**
-     * Get the value associated with a particular key.
-     *
-     * @throws Exception if the key is not in the dictionary.
-     */
-    public V get(K key) throws Exception;
+  /**
+   * Get the value associated with a particular key.
+   *
+   * @throws Exception if the key is not in the dictionary.
+   */
+  public V get(K key)
+    throws Exception;
 
-    /**
-     * Determine if the dictionary contains a value with a particular
-     * key.  (Useful in checking the precondition for get.)
-     */
-    public boolean containsKey(K key);
+  /**
+   * Determine if the dictionary contains a value with a particular
+   * key.  (Useful in checking the precondition for get.)
+   */
+  public boolean containsKey(K key);
 
-    // +----------+--------------------------------------------------------
-    // | Mutators |
-    // +----------+
+  // +----------+--------------------------------------------------------
+  // | Mutators |
+  // +----------+
 
-    /**
-     * Set the value associated with a particular key.  If there was
-     * previously a value with that key, replaces it.
-     */
-    public void set(K key, V value);
+  /**
+   * Set the value associated with a particular key.  If there was
+   * previously a value with that key, replaces it.
+   */
+  public void set(K key, V value);
 
-    /**
-     * Remove the value associated with a particular key.  Future calls
-     * to get with the same key should throw exceptions (at least until
-     * the next call to set with the same key).
-     *
-     * @return the value associated with the key
-     * @throws Exception if the key is not in the dictionary
-     */ 
-    public V remove(K key) throws Exception;
+  /**
+   * Remove the value associated with a particular key.  Future calls
+   * to get with the same key should throw exceptions (at least until
+   * the next call to set with the same key).
+   *
+   * @return the value associated with the key
+   * @throws Exception if the key is not in the dictionary
+   */
+  public V remove(K key)
+    throws Exception;
 
-    /**
-     * Remove all of the items from the dictionary.
-     */
-    public void clear();
+  /**
+   * Remove all of the items from the dictionary.
+   */
+  public void clear();
 
-    // +-----------+-------------------------------------------------------
-    // | Iterators |
-    // +-----------+
+  // +-----------+-------------------------------------------------------
+  // | Iterators |
+  // +-----------+
 
-    /**
-     * Get an iterable for the values in the dictionary.  The values are
-     * iterated in no particular order.  If a value appears more than once
-     * in the dictionary (i.e., if it is associated with more than one key),
-     * it appears more than once in the iteration.
-     */
-    public Iterator<V> iterator();
+  /**
+   * Get an iterable for the values in the dictionary.  The values are
+   * iterated in no particular order.  If a value appears more than once
+   * in the dictionary (i.e., if it is associated with more than one key),
+   * it appears more than once in the iteration.
+   */
+  public Iterator<V> iterator();
 
-    /**
-     * Get an iterable for the keys in the dictionary.  The keys are
-     * iterated in no particular order.  (We want an iterable so that
-     * we can use it with Java's for-each form.)
-     */
-    public Iterable<K> keys();
+  /**
+   * Get an iterable for the keys in the dictionary.  The keys are
+   * iterated in no particular order.  (We want an iterable so that
+   * we can use it with Java's for-each form.)
+   */
+  public Iterable<K> keys();
 
-    /**
-     * Get an iterator for the keys in the dictionary.
-     */
-    public Iterator<K> keysIterator();
+  /**
+   * Get an iterator for the keys in the dictionary.
+   */
+  public Iterator<K> keysIterator();
 
 } // Dictionary<K,V>
